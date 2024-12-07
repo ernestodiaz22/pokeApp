e88d89760ca4383936e4ba0ec812ce862fd3ed43
Marc <marc.rubio98@gmail.com>
2024-12-07 17:52:33 +0100

    Pantalla terminada con item creado, pendiente merge

--- appPokedex/app/src/main/java/com/example/apppokedex/MainActivity.kt
+++ appPokedex/app/src/main/java/com/example/apppokedex/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.apppokedex
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Navegar directamente a la pantalla de Buscador
+        val intent = Intent(this, buscador::class.java)
+        startActivity(intent)
+
+        // Finalizar MainActivity para evitar regresar a ella
+        finish()
     }
 }
---
+++ appPokedex/app/src/main/java/com/example/apppokedex/item.kt
@@ -0,0 +1,5 @@
+package com.example.apppokedex
+
+class item {
+
+}
