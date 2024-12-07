45bd11699df03ad52d34de2ea2b1981cebea7bdc
Marc <marc.rubio98@gmail.com>
2024-12-07 20:00:05 +0100

    Preparado para merge en principal

--- appPokedex/app/src/main/java/com/example/apppokedex/Login.kt
+++ appPokedex/app/src/main/java/com/example/apppokedex/Login.kt
@@ -1,6 +1,8 @@
 package com.example.apppokedex
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,15 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Encontrar el botón en la interfaz
+        val button = findViewById<Button>(R.id.button) // Cambia R.id.button si el id del botón es diferente
+
+        // Configurar el listener para el botón
+        button.setOnClickListener {
+            // Crear el Intent para navegar a la actividad de Inicio
+            val intent = Intent(this, Inicio::class.java)
+            startActivity(intent)
+            finish() // Cierra la actividad Login si ya no se necesita
         }
     }
 }
